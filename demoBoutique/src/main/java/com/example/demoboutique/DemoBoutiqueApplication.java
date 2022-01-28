305893eae2d9515270b619572ed3bcbfa1463089
tomavo <tomavoclarisse@gmail.com>
2022-01-28 16:58:02 +0000

    first commit

---
+++ demoBoutique/src/main/java/com/example/demoboutique/DemoBoutiqueApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoboutique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBoutiqueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoBoutiqueApplication.class, args);
+    }
+
+}
---
+++ demoBoutique/src/test/java/com/example/demoboutique/DemoBoutiqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoboutique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBoutiqueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
