1f7ec0a5b567a2663e9e7543111e3b372e11255e
ulrichfidel <fidelaihounkpe@gmail.com>
2022-01-29 13:20:43 +0100

    Création des classes, components et services Angular

--- boutique/src/app/app-routing.module.ts
+++ boutique/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ListeProduitsComponent} from "./components/produits/liste-produits/liste-produits.component";
+import {AddProduitComponent} from "./components/produits/add-produit/add-produit.component";
+import {AddCategorieComponent} from "./components/categories-produits/add-categorie/add-categorie.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: 'produits/liste', component: ListeProduitsComponent },
+  { path: 'produits/ajout', component: AddProduitComponent },
+  { path: 'produits/modifier/:produitId', component: AddProduitComponent },
+  { path: 'produits/categorie/ajout', component: AddCategorieComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- boutique/src/app/app.module.ts
+++ boutique/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddProduitComponent } from './components/produits/add-produit/add-produit.component';
+import { ListeProduitsComponent } from './components/produits/liste-produits/liste-produits.component';
+import { AddCategorieComponent } from './components/categories-produits/add-categorie/add-categorie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddProduitComponent,
+    ListeProduitsComponent,
+    AddCategorieComponent
   ],
   imports: [
     BrowserModule,
